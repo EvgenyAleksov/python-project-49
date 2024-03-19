cb2cdc289316c43ba4b8e6ad363c4c7459875e62
Evgeny <info@tinamagic.lv>
2024-03-19 12:53:56 +0200

    Step 8

--- brain_games/answer.py
+++ brain_games/answer.py
@@ -1,26 +1,12 @@
 def answer(name, task):
-    if task == 'even':
-        from brain_games.scripts.tasks.even import task
-    if task == 'calc':
-        from brain_games.scripts.tasks.calc import task
-    if task == 'even':
-        from brain_games.scripts.tasks.gcd import task
-    if task == 'progression':
-        from brain_games.scripts.tasks.progression import task
+    correct_answer = task
+    my_answer = input()
+    print('Your answer: ', my_answer)
 
-    stage = 1
-    while stage <= 3:
-        correct_answer = task()
-        answer = input()
-        print('Your answer: ' + answer)
-
-        if answer == correct_answer:
-            print('Correct!')
-            if stage == 3:
-                print('Congratulations, ' + name + '!')
-        else:
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '"
-                  + str(correct_answer) + "'.\nLet's try again, " + name + "!")
-            break
-
-        stage = stage + 1
+    if my_answer == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print("'" + my_answer + "' is wrong answer ;(. Correct answer was '"
+              + str(correct_answer) + "'.\nLet's try again, " + name + "!")
+        return False
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,8 +1,14 @@
 from brain_games.greeting import greeting
+from brain_games.scripts.tasks.progression import task
 from brain_games.answer import answer
 
 
 def main():
     name = greeting()
     print('What number is missing in the progression?')
-    answer(name, 'progression')
+
+    for i in range(1, 4):
+        if answer(name, task()) is False:
+            break
+        elif i == 3:
+            print('Congratulations, ' + name + '!')
