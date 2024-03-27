1628ad6ae3b8bbf506d39e188d0baa4916df24c4
Evgeny <info@tinamagic.lv>
2024-03-27 18:19:20 +0200

    Сheck

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from brain_games.games.gcd import calculate
 from brain_games.games.gcd import set_task
 from brain_games.engine import play
@@ -5,3 +7,7 @@ from brain_games.engine import play
 
 def main():
     play(set_task, calculate)
+
+
+if __name__ == '__main__':
+    main()
